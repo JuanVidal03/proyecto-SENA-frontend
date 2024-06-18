8e9365b92de8991fb59eb5b12925e883b2ea03c0
Juan Vidal <juanmvg2003@gmail.com>
2024-06-18 18:34:02 -0500

    cambio de css a tailwind

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import imgLogin from "../../../public/imgLogin.webp";
+import logo from "../../../public/logo-indestec.png"
 import "./login.css";
 
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+
   const navigate = useNavigate();
   const {
     handleSubmit,
@@ -17,27 +19,34 @@ export default function Login() {
     console.log(data);
     navigate("/");
   });
+
+
   return (
-    <section className="container">
-      <div className="container-login">
-        <div className="main-login">
-          <div className="title-login">
-            <h2>here logo</h2>
+    <section className="w-full h-screen relative overflow-hidden flex justify-center items-center">
+      <div className="bg-white w-full max-w-[1000px] h-[75%] flex relative z-[9] rounded-xl shadow-login">
+        <div className="w-full max-w-[50%] p-4 flex items-center justify-center flex-col">
+          {/* 
+          <div className="w-full flex items-center justify-center">
+            <div className="bg-azul-fuerte p-5 rounded-full">
+              <img className="w-[100px]" src={`${logo}`} alt="Logo de Indestec" />
+            </div>
           </div>
-          <div className="section-form">
-            <form onSubmit={clickBotonIniciar}>
-              <div className="label-input">
-                <label htmlFor="user">User:</label>
+          */}
+          <div className="text-azul-fuerte w-full h-[60%] flex justify-center items-center">
+            <form className="w-[60%] flex flex-col" onSubmit={clickBotonIniciar}>
+              <div className="flex flex-col mb-4">
+                <label className="mb-1 font-bold" htmlFor="user">Correo Eléctronico:</label>
                 <input
-                  type="text"
+                  className="p-2 rounded-lg text-[1rem] border border-azul-fuerte focus:outline-none transition-all focus:bg-gray-100"
+                  type="email"
                   id="email"
                   name="email"
+                  autoComplete="off"
                   {...register("email", {
                     required: {
                       value: true,
                       message: "Por favor ingrese el correo",
                     },
-
                     pattern: {
                       value: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/,
                       message: "Por favor ingrese un correo valido",
@@ -45,17 +54,18 @@ export default function Login() {
                   })}
                 />
                 {errors.email && (
-                  <span className="text-danger-form">
+                  <span className="text-red-500">
                     {" "}
                     {errors.email.message}{" "}
                   </span>
                 )}
               </div>
 
-              <div className="label-input">
-                <label htmlFor="password">Password:</label>
+              <div className="flex flex-col mb-4">
+                <label className="mb-1 font-bold" htmlFor="password">Contraseña:</label>
                 <input
-                  type="text"
+                  className="p-2 rounded-lg text-[1rem] border border-azul-fuerte focus:outline-none transition-all focus:bg-gray-100"
+                  type="password"
                   id="password"
                   name="password"
                   {...register("password", {
@@ -71,32 +81,34 @@ export default function Login() {
                   })}
                 />
                 {errors.password && (
-                  <span className="text-danger-form">
+                  <span className="text-red-500">
                     {errors.password.message}
                   </span>
                 )}
               </div>
 
-              <div className="div-button">
-                <button type="submit" value="Enviar">
-                  Login
+              <div className="flex justify-center items-center">
+                <button className="border-none bg-azul-fuerte font-bold text-white text-[1rem] cursor-pointer py-3 w-full rounded-lg mt-2" type="submit">
+                  Ingresa
                 </button>
               </div>
             </form>
           </div>
         </div>
-        <figure className="aside-img-login">
-          <img className="img-login" src={imgLogin} alt="" />
+        <figure className="w-full relative max-w-[50%] after:w-full after:h-full after:absolute after:left-0 after:top-0 after:bg-azul-gradiente after:content-[''] after:z-[2] after:rounded-tr-xl after:rounded-br-xl">
+          <img className="w-full h-full object-cover z-[1] absolute rounded-tr-xl rounded-br-xl" src={imgLogin} alt="" />
         </figure>
       </div>
-      <div className="back">
-        <div className="top">
-          <div className="circle-top"></div>
+      {/* efectos de los circulos */}
+      <div className="h-full w-full flex justify-between flex-col absolute bg-gris-suave">
+        <div>
+          <div className="circle-top animate-bounce-login"></div>
         </div>
-        <div className="bottom">
-          <div className="circle-bottom"></div>
+        <div className="flex justify-end">
+          <div className="circle-bottom animate-bounce-login"></div>
         </div>
       </div>
+      {/* fin efectos de los cirulos */}
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'azul-fuerte': '#000074',
+        'azul-gradiente': '#0000749f',
+        'gris-suave': '#f2f2f2'
+      },
+      boxShadow:{
+        'login': '0px 0px 10px rgba(0, 0, 0, 0.2)'
+      },
+      keyframes:{
+        'bounce-login': {
+          '0%, 100%': {transform: 'translateY(15px) translateX(15px)'},
+          '50%': {transform: 'translateY(0px) translateX(0px)'},
+        },
+      },
+      animation:{
+        'bounce-login': 'bounce-login 3s infinite'
+      }
+    },
   },
   plugins: [],
 }
