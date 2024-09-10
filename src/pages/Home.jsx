120d222f67333d74a4cc83549d5fc49c4da536fe
Juan Vidal <juanmvg2003@gmail.com>
2024-09-09 22:17:57 -0500

    home services section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import { Suspense, lazy } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBullseye, faMountainSun } from "@fortawesome/free-solid-svg-icons";
+import {
+    faBullseye,
+    faMountainSun,
+    faSeedling,
+    faTractor,
+    faRecycle
+} from "@fortawesome/free-solid-svg-icons";
 
 const Loader = lazy(() => import("../components/loader/Loader.jsx"));
 const HomeNavBar = lazy(() => import("../components/HomeNavBar.jsx"));
@@ -59,27 +65,29 @@ const Home = () => {
                                 </figure>
                                 <div className="w-[50%] h-full flex flex-col gap-8 justify-center items-center">
 
-                                    <article className="w-full flex flex-col gap-8 max-w-[70%] p-8 transition-all bg-custom-purple hover:translate-x-3">
-                                        <div className="flex justify-center items-center">
-                                            <FontAwesomeIcon className="text-7xl" icon={faBullseye}/>
-                                        </div>
-                                        <div className="flex flex-col gap-4 justify-center items-center">
-                                            <h5 className="font-semibold text-3xl">Misión</h5>
-                                            <p className="text-lg text-center">La misión de Zenit Code es liderar la transformación digital del sector agroalimentario, proporcionando soluciones innovadoras y personalizadas que optimizan procesos, aumentan la productividad, garantizan la sostenibilidad y empoderan a los agricultores, contribuyendo a un futuro alimentario seguro y sostenible.</p>
-                                        </div>
-                                    </article>
-
-                                    <article className="w-full flex flex-col gap-8 max-w-[70%] border-white border p-8 transition-all hover:translate-x-3">
-                                        <div className="flex justify-center items-center">
-                                            <FontAwesomeIcon className="text-7xl" icon={faMountainSun}/>
-                                        </div>
-                                        <div className="flex flex-col gap-4 justify-center items-center">
-                                            <h5 className="font-semibold text-3xl">Visión</h5>
-                                            <p className="text-lg text-center">
-                                            La visión de Zenit Code es liderar un futuro donde la agricultura esté conectada tecnológicamente, equilibrando eficiencia y sostenibilidad con un mínimo impacto ambiental, mejorando la vida de las personas y el planeta.</p>
+                                    <article className="border-2 border-custom-purple max-w-[70%] bg-transparent group relative inline-flex focus:outline-none w-full sm:w-auto">
+                                        <div className=" flex-col gap-8 w-full text-white inline-flex items-center justify-center self-stretch px-4 py-12 text-lg text-center font-medium bg-custom-purple ring-1 ring-custom-purple ring-offset-1 ring-offset-custom-purple transform transition-all group-hover:-translate-y-3 group-hover:translate-x-3 group-focus:-translate-y-1 group-focus:-translate-x-1">
+                                            <div className="flex justify-center items-center">
+                                                <FontAwesomeIcon className="text-7xl" icon={faMountainSun}/>
+                                            </div>
+                                            <div className="flex flex-col gap-4 justify-center items-center">
+                                                <h5 className="font-semibold text-3xl">Misión</h5>
+                                                <p className="text-lg text-center">La misión de Zenit Code es liderar la transformación digital del sector agroalimentario, proporcionando soluciones innovadoras y personalizadas que optimizan procesos, aumentan la productividad, garantizan la sostenibilidad y empoderan a los agricultores, contribuyendo a un futuro alimentario seguro y sostenible.</p>
+                                            </div>
                                         </div>
                                     </article>
 
+                                    <article className="border-2 border-white max-w-[70%] bg-transparent group relative inline-flex focus:outline-none w-full sm:w-auto">
+                                        <div className="flex-col gap-8 w-full text-white inline-flex items-center justify-center self-stretch px-4 py-12 text-lg text-center font-medium bg-transparent ring-1 ring-white ring-offset-1 ring-offset-white transform transition-all group-hover:-translate-y-3 group-hover:translate-x-3 group-focus:-translate-y-1 group-focus:-translate-x-1 hover:bg-[#ffffff80] hover:backdrop-blur-sm">
+                                            <div className="flex justify-center items-center">
+                                                <FontAwesomeIcon className="text-7xl" icon={faBullseye}/>
+                                            </div>
+                                            <div className="flex flex-col gap-4 justify-center items-center">
+                                                <h5 className="font-semibold text-3xl">Visión</h5>
+                                                <p className="text-lg text-center">La visión de Zenit Code es liderar un futuro donde la agricultura esté conectada tecnológicamente, equilibrando eficiencia y sostenibilidad con un mínimo impacto ambiental, mejorando la vida de las personas y el planeta.</p>
+                                            </div>
+                                        </div>
+                                    </article>
                                 </div>
                             </div>
 
@@ -88,21 +96,68 @@ const Home = () => {
                     </section>
 
                     <section id="services" className="w-ful pt-[5.5rem] flex flex-col items-center">
-                        <div className="w-full flex flex-col gap-16 max-w-[1300px]">
+                        <div className="w-full flex flex-col gap-12 max-w-[1300px]">
                             <div className="w-full flex flex-col justify-center items-center">
                                 <h2 className="text-5xl text-center font-semibold">Nuestros Servicios</h2>
                                 <p className="mt-5 text-center text-lg w-full max-w-[1000px]">Ofrecemos soluciones tecnológicas innovadoras para el sector agroalimentario, incluyendo plataformas de gestión agrícola conectada, automatización de procesos, monitoreo remoto de cultivos y riego inteligente. Empoderamos a los agricultores con herramientas personalizadas que optimizan la productividad y minimizan el impacto ambiental.</p>
                             </div>
 
-                            <div>
-                                
+                            <div className="grid grid-cols-3 gap-5">
+                                <div className="border-2 border-custom-purple bg-transparent group relative inline-flex focus:outline-none w-full sm:w-auto">
+                                    <span className="flex-col gap-8 w-full text-white inline-flex items-center justify-center self-stretch px-4 py-8 text-lg text-center font-medium bg-custom-purple ring-1 ring-custom-purple ring-offset-1 ring-offset-custom-purple transform transition-transform group-hover:-translate-y-3 group-hover:translate-x-3 group-focus:-translate-y-1 group-focus:-translate-x-1">
+                                        <figure>
+                                            <FontAwesomeIcon className="text-7xl" icon={faSeedling}/>
+                                        </figure>
+                                        <div className="flex flex-col gap-4">
+                                            <h6 className="font-bold text-2xl">Automatización de Procesos Agrícolas</h6>
+                                            <p>Desarrollo de software para automatizar tareas como la siembra, riego, cosecha, y el monitoreo de cultivos. Implementación de sensores IoT para recopilar datos en tiempo real de los campos y optimizar el uso de recursos como agua y fertilizantes.</p>
+                                        </div>
+                                    </span>
+                                </div>
+
+                                <div className="border-2 border-white bg-transparent group relative inline-flex focus:outline-none w-full sm:w-auto">
+                                    <span className="flex-col gap-8 w-full text-white inline-flex items-center justify-center self-stretch px-4 py-12 text-lg text-center font-medium bg-transparent ring-1 ring-white ring-offset-1 ring-offset-white transform transition-all group-hover:-translate-y-3 group-hover:translate-x-3 group-focus:-translate-y-1 group-focus:-translate-x-1 hover:bg-[#ffffff80] hover:backdrop-blur-sm">
+                                        <figure>
+                                            <FontAwesomeIcon className="text-7xl" icon={faTractor}/>
+                                        </figure>
+                                        <div className="flex flex-col gap-4">
+                                            <h6 className="font-bold text-2xl">Sistemas de Gestión de Fincas</h6>
+                                            <p>Soluciones personalizadas para el control de inventarios, planificación de cosechas, gestión de recursos y monitoreo del rendimiento de los cultivos. Paneles de control en tiempo real que brindan a los agricultores datos sobre el rendimiento y la productividad de sus operaciones.</p>
+                                        </div>
+                                    </span>
+                                </div>
+
+
+                                <div className="border-2 border-custom-purple bg-transparent group relative inline-flex focus:outline-none w-full sm:w-auto">
+                                    <span className="flex-col gap-8 w-full text-white inline-flex items-center justify-center self-stretch px-4 py-2 text-lg text-center font-medium bg-custom-purple ring-1 ring-custom-purple ring-offset-1 ring-offset-custom-purple transform transition-transform group-hover:-translate-y-3 group-hover:translate-x-3 group-focus:-translate-y-1 group-focus:-translate-x-1">
+                                        <figure>
+                                            <FontAwesomeIcon className="text-7xl" icon={faRecycle}/>
+                                        </figure>
+                                        <div className="flex flex-col gap-4">
+                                            <h6 className="font-bold text-2xl">Sistemas de Sostenibilidad y Gestión Ambiental</h6>
+                                            <p>Implementación de soluciones tecnológicas para la gestión eficiente de los recursos naturales, como el agua y la energía. Desarrollo de herramientas que midan el impacto ambiental de las operaciones agrícolas y proporcionen recomendaciones para reducirlo.</p>
+                                        </div>
+                                    </span>
+                                </div>
+
+                            </div>
+                        </div>
+                    </section>
+
+                    <section id="developers" className="w-ful pt-[5.5rem] flex flex-col items-center">
+                        <div className="w-full flex flex-col gap-12 max-w-[1300px]">
+                            <div className="w-full flex flex-col justify-center items-center">
+                                <h2 className="text-5xl text-center font-semibold">Conoce Nuestro Equipo</h2>
+                                <p className="mt-5 text-center text-lg w-full max-w-[1000px]">En <span className="font-bold">Zenit Code</span>, contamos con un equipo de desarrolladores apasionados y altamente capacitados que se especializan en transformar ideas en soluciones tecnológicas. Cada miembro de nuestro equipo aporta una combinación única de experiencia técnica y creatividad, asegurando que los proyectos sean entregados con la más alta calidad y dentro de los plazos establecidos.</p>
                             </div>
                         </div>
                     </section>
                 </main>
 
 
-                <footer> pie de pagina</footer>
+                <footer className="w-full flex justify-center items-center py-4">
+                    <p className="text-gray-500">© {new Date().getFullYear()} <span className="text-white font-semibold">{import.meta.env.VITE_COMPANY_NAME}</span>. Todos los derechos reservados.</p>
+                </footer>
             </div>
         </Suspense>
 
